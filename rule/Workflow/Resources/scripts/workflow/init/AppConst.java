788cc202ee962ba0bbe7d19d35784d3a92cc08ab
k-zon_000 <k-zon_000@KayraNote>
2016-04-26 11:22:09 +0300

    refactor

--- rule/Workflow/Resources/scripts/workflow/init/AppConst.java
+++ rule/Workflow/Resources/scripts/workflow/init/AppConst.java
@@ -6,7 +6,7 @@ public class AppConst {
 	public static String NAME = "Workflow";
 	public static String NAME_ENG = "Workflow";
 	public static String NAME_RUS = "Служебные записки";
-	public static String NAME_KZ = "Служебные записки";
+	public static String NAME_KAZ = "Служебные записки";
 	public static AppCode CODE = AppCode.WORKSPACE;
 	public static String DEFAULT_URL = "p?id=officememo-view";
 	public static String FT_INDEX_SCOPE = "";
