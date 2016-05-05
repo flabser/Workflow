21e3c883284c23883fe6adc07a2eb7ece0eb84fd
k-zon_000 <k-zon_000@KayraNote>
2016-05-05 12:39:51 +0300

    incoming

---
+++ rule/Workflow/Resources/scripts/workflow/model/Incoming.java
@@ -0,0 +1,49 @@
+package workflow.model;
+
+/**
+ * 
+ * @author Kayra created 07-04-2016
+ */
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.exponentus.common.model.Attachment;
+import com.exponentus.dataengine.jpa.SecureAppEntity;
+
+import staff.model.Organization;
+
+@Entity
+@Table(name = "incomings")
+@NamedQuery(name = "Incoming.findAll", query = "SELECT m FROM Incoming AS m ORDER BY m.regDate")
+public class Incoming extends SecureAppEntity<UUID> {
+
+	@Column(name = "reg_number")
+	private String regNumber;
+
+	@Column(name = "applied_reg_date")
+	private Date appliedRegDate;
+
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+	@JoinTable(name = "incoming_attachments", joinColumns = { @JoinColumn(name = "incoming_id", referencedColumnName = "id") }, inverseJoinColumns = {
+	        @JoinColumn(name = "attachment_id", referencedColumnName = "id") })
+	private List<Attachment> attachments = new ArrayList<>();
+
+	private Organization sender;
+
+	@Column(nullable = false)
+	private String summary = "";
+
+}
